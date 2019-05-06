75f59bb8f92af4261c065f1b782b3c520286efc4
alanfanstw <alanfans@alumni.npust.edu.tw>
2019-05-06 13:57:40 +0800

    update python

--- springboot-dubbo-server/springboot-dubbo-provider/src/test/java/com/example/demo/test.py
+++ springboot-dubbo-server/springboot-dubbo-provider/src/test/java/com/example/demo/test.py
@@ -3,8 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
-
-class Am_static():
+class Am_auto():
 	account = {
 		'email' : '123@6vip.ml',
 		'passwd' : ':*)?8A2f4Fck',
@@ -29,8 +28,6 @@ class Am_static():
 		else:
 			self.brower = webdriver.Firefox(executable_path='E:\Program Files\selenium\geckodriver.exe')
 
-		
-
 	def login(self):
 		self.brower.get(self.em_url.get('login'))
 		# print("firefox:"+fbrower.page_source)
@@ -51,22 +48,26 @@ class Am_static():
 			self.login()
 		
 	def visite_earn_page_scroll(self):
-    	# not find earn_pgae
-		# button_earn = brower.find_element(By.LINK_TEXT,self.em_url.get("earn"))
-		# button_earn.click()
-		# brower.get(self.em_url.get('earn'))5,261
-		self.brower.get(self.em_url.get('earn'))
-		js = 'document.getElementsByClassName("main-panel")[0].scrollTop'
-		height = self.brower.execute_script("return "+js)
-		print('height:'+str(height))
-		self.brower.execute_script(js+"=10000")
-
+		try:
+	   		# not find earn_pgae
+			# button_earn = brower.find_element(By.LINK_TEXT,self.em_url.get("earn"))
+			# button_earn.click()
+			# brower.get(self.em_url.get('earn'))5,261
+			self.brower.get(self.em_url.get('earn'))
+			js = 'document.getElementsByClassName("main-panel")[0].scrollTop'
+			height = self.brower.execute_script("return "+js)
+			self.brower.execute_script(js+"="+str(height+10000))
+			print('height:'+str(height+10000))
+		except BaseException as identifier:
+	   		# self.brower.refresh()
+	   		self.visite_earn_page_scroll()
+		
 	def checkGV(self):
 		time.sleep(6)
 		gvs_div = self.brower.execute_script("return $('.timeline-panel')")
 		hasGV = 0
 		for gv in gvs_div:
-			if '5 G' in gv.text:
+			if '5 Good Votes from' in gv.text:
 				try:
 					ads = gv.find_element(*self.em_botton.get('ads'))
 					self.getGV(ads)
@@ -74,15 +75,18 @@ class Am_static():
 				except BaseException as identifier:
 					print("error")
 				
-
 		if hasGV == 0:
 			self.visite_earn_page_scroll()
 			self.checkGV()
 
 	def getGV(self,ads):
 		ads.click()
-		goodconfirm.click()
 		time.sleep(21)
+		# switch_tab
+		windows = self.brower.window_handles
+		self.brower.switch_to.window(windows[1])
+		self.brower.close()
+		self.brower.switch_to.window(windows[0])
 		goodconfirm = self.brower.find_element_by_class_name("swal2-confirm")
 		goodconfirm.click()
 		goodconfirm = self.brower.find_element_by_class_name("swal2-confirm")
@@ -90,9 +94,7 @@ class Am_static():
 		time.sleep(3)
 		self.checkGV()
 
-		
-
-test = Am_static('chrome')
+test = Am_auto('chrome')
 test.login()
 test.visite_earn_page_scroll()
 test.checkGV()
